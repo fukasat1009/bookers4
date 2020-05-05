41a81ba94414dc0ee06ea6b61f0bca810c88efd7
津田恭平 <tsudakyouhei@kumadon.local>
2020-05-05 16:13:02 +0900

    first-commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,11 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+
+Refile.secret_key = 'a4bfba026b237ce682df1c5b63b503f34008124b6ab0b9c3b48c35460388c306ec2ffd2a0c2638637808d8445cdaefbfcd107b9aa95956677fc13d97103a2323'
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+
+    devise_for :users
+	root to: "home#index"
+
+    get "home/about" => "home#about"
+
+    resources :users
+end
