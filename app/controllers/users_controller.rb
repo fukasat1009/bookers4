daec1f0f25ab110914fdb2db6c78dcb5a8d7c243
津田恭平 <fukagawa.sat1009@gmail.com>
2020-05-06 23:43:43 +0900

    up date3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,14 @@
 class UsersController < ApplicationController
   def index
   	@users = User.all
+    @post = Book.new
+
   end
 
   def show
   	@user = User.find(params[:id])
     @post = Book.new
+    
   end
 
   def edit
